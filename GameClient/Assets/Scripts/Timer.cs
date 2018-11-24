cd13f91f8e6b7a42533ba21cdf39623afa641d82
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-24 19:37:01 -0400

    added a timer object to the top of the screen.

---
+++ GameClient/Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+  public int timeLeft = 0;
+  public Text countdownText;
+
+  public void Init(int time) {
+    timeLeft = time;
+  }
+	
+	void Start () {
+    StartCoroutine(LoseTime());
+	}
+	
+	void Update () {
+    GetComponent<Text>().text = "" + timeLeft;
+    if (isTimerOff())
+    {
+      GetComponent<Text>().text = "Time off";
+      SceneManager.LoadScene("End");
+    }
+  }
+
+  public bool isTimerOff() {
+    if (timeLeft > 0) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  IEnumerator LoseTime()
+  {
+    while (true)
+    {
+      yield return new WaitForSeconds(1);
+      timeLeft--;
+    }
+  }
+}
