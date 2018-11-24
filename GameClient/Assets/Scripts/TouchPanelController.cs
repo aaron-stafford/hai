823330a8b33accbf09eab24a2e5d2f76b7d34bfb
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-23 22:28:38 -0400

    Added simple start UI scene and Game scene.

---
+++ GameClient/Assets/Scripts/TouchPanelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TouchPanelController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+  void Start() {
+    Debug.Log("Started");
+  }
+
+  public void OnPointerDown(PointerEventData eventData) {
+    Debug.Log("Touched Down");
+  }
+
+  public void OnPointerUp(PointerEventData eventData) {
+    Debug.Log("Touched Up");
+    SceneManager.LoadScene("Game");
+  }
+}
