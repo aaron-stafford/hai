41ea8a063fa47cd8737d0c89011b9713d89e3526
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-24 12:02:20 -0400

    Demo for left and right selector.

---
+++ GameClient/Assets/Scripts/ChoiceTouchController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ChoiceTouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+  private GameObject m_LeftObject; 
+  private GameObject m_RightObject; 
+
+	void Start () {
+	  m_LeftObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    m_LeftObject.transform.position = new Vector3(-1.9f, 0.0f, 0.0f);	
+	  m_RightObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    m_RightObject.transform.position = new Vector3(1.9f, 0.0f, 0.0f);	
+	}
+
+	void Update () {
+	}
+
+  public void OnPointerDown(PointerEventData eventData) {
+  }
+
+  public void OnPointerUp(PointerEventData eventData) {
+    if( eventData.position.x < Screen.width * 0.5 ) {
+      Debug.Log("Left side");
+    }
+    else {
+      Debug.Log("Right side");
+    }
+  }
+}
