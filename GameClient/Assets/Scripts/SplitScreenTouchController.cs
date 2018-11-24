763fb307cb4fa960bc282cf66b141572c0527ee1
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-24 11:36:42 -0400

    added ability to detect if the player clicked on the left or right side of the screen.

---
+++ GameClient/Assets/Scripts/SplitScreenTouchController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SplitScreenTouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+  public void OnPointerDown(PointerEventData eventData) {
+  }
+
+  public void OnPointerUp(PointerEventData eventData) {
+    if( eventData.position.x < Screen.width * 0.5 ) {
+      Debug.Log("Left side");
+    }
+    else {
+      Debug.Log("Right side");
+    }
+  }
+}
