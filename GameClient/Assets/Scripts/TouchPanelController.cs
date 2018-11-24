5c82cd96535a7a748b6c0185dd500aae1e3b6a1f
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-23 22:59:18 -0400

    Added End Scene and refactor.

--- GameClient/Assets/Scripts/TouchPanelController.cs
+++ GameClient/Assets/Scripts/TouchPanelController.cs
@@ -6,17 +6,10 @@ using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class TouchPanelController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
-
-  void Start() {
-    Debug.Log("Started");
-  }
-
   public void OnPointerDown(PointerEventData eventData) {
-    Debug.Log("Touched Down");
   }
 
   public void OnPointerUp(PointerEventData eventData) {
-    Debug.Log("Touched Up");
     SceneManager.LoadScene("Game");
   }
 }
