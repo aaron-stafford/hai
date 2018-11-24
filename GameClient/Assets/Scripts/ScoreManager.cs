2d4fe38752f17c75131e58c37f9f589b0c872a9f
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-24 18:44:33 -0400

    added first draft of a score manager.

---
+++ GameClient/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿public class ScoreManager  {
+    private static ScoreManager _instance;
+    public static ScoreManager Instance { get { return _instance; } }
+    private int m_Score = 0;
+    private int m_MatchReward = 10;
+
+    public void Init() {
+      m_Score = 100;
+    }
+
+    public void UsedATap() {
+      m_Score--;
+    }
+
+    public void FoundAMatch() {
+      m_Score += m_MatchReward;
+    }
+
+    public void FoundAMatch(int a_MatchReward) {
+      m_Score += a_MatchReward;
+    }
+
+    public bool AreWeDead() {
+      if(m_Score <= 0) {
+        return true;
+      }
+      return false;
+    }
+}
