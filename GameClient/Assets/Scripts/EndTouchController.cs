62c0489eb4445d95bb26fe0b84d0357fa7576b2c
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-25 14:36:39 -0400

    added code to show the credits screen.

--- GameClient/Assets/Scripts/EndTouchController.cs
+++ GameClient/Assets/Scripts/EndTouchController.cs
@@ -6,10 +6,24 @@ using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class EndTouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler  {
+  private bool m_TouchingDown = false;
+  void Start() {
+    m_TouchingDown = false;
+  }
   public void OnPointerDown(PointerEventData eventData) {
+    m_TouchingDown = true;
+    StartCoroutine(EasterEgg());
   }
 
   public void OnPointerUp(PointerEventData eventData) {
     SceneManager.LoadScene("Gamev2");
+    m_TouchingDown = false;
+  }
+
+  IEnumerator EasterEgg () {
+    yield return new WaitForSeconds (3);
+    if(m_TouchingDown) {
+      SceneManager.LoadScene("Credits");
+    }
   }
 }
