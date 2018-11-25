b8dc31a643aa2aeaab7e651a39a9f4dcf41ee42e
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-25 13:45:26 -0400

    added a touch controller for the win screen.
    Can now touch on win screen to play again.

---
+++ GameClient/Assets/Scripts/WinTouchController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class WinTouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler  {
+  public void OnPointerDown(PointerEventData eventData) {
+  }
+
+  public void OnPointerUp(PointerEventData eventData) {
+    SceneManager.LoadScene("Gamev2");
+  }
+}
