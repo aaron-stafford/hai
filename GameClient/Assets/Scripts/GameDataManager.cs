bd62443f825b5665e875055cffbc8c0f9b2c0714
Aaron Stafford <aaron@staffordfarm.com.au>
2018-11-26 23:06:56 -0400

    Made a better singleton for the game data management

--- GameClient/Assets/Scripts/GameDataManager.cs
+++ GameClient/Assets/Scripts/GameDataManager.cs
@@ -3,21 +3,44 @@ using System.Collections.Generic;
 using System.IO; 
 using UnityEngine;
 
+// Thread safe Singleton
+// https://www.c-sharpcorner.com/UploadFile/8911c4/singleton-design-pattern-in-C-Sharp/
+// Interface between data from data.json and how accessed in code.
 public class GameDataManager {
   public GameData gameData;
-  private static string gameDataFileName = "data.json";
 
+  // Singleton creation code
+  GameDataManager() {}
+  private static readonly object padlock = new object();  
+  private static GameDataManager instance = null;  
+  public static GameDataManager Instance {
+    get {
+      lock (padlock) {
+        if (instance == null) {
+          instance = new GameDataManager();  
+          // Choosing to load data at this point out of convenience
+          instance.LoadData();
+        }
+        return instance;
+      }
+    }
+  }
+  // End of singleton creation code
+
+  private static string gameDataFileName = "data.json";
+/*
   private static GameDataManager instance;
   public static GameDataManager Instance {
     get {  
       if (instance == null) {  
         instance = new GameDataManager();  
+        instance.Init();
       }  
       return instance;  
     } 
   }
-
-  public void Init() {
+*/
+  public void LoadData() {
     string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
     if(File.Exists(filePath)) {
       string dataAsJson = File.ReadAllText(filePath); 
--- GameClient/Assets/Scripts/MatchGameController.cs
+++ GameClient/Assets/Scripts/MatchGameController.cs
@@ -6,10 +6,6 @@ public class MatchGameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-    // Initialize the GameDataManager.
-    // TODO: I think there is a better way to manage this...
-    GameDataManager.Instance.Init();
-
     // Reset the match manager.
     // TODO: There should be a better place for this.
     MatchManager.Instance.Reset();
